e6c546b8baee9ffa6026588cc0f0317a9508724c
Pablo Santana <pablosntojd@gmail.com>
2023-06-07 17:58:02 +0100

    added more test

--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -1,6 +1,9 @@
-import {strictEqual} from "node:asset"
-import {stringCalculator} from "./stringCalculator.js"
+import { strictEqual } from "node:assert";
+import { stringCalculator } from "./stringCalculator.js";
 
 strictEqual(stringCalculator("1,2"), 3);
 strictEqual(stringCalculator("2,3"), 5);
 strictEqual(stringCalculator("12,3"), 15);
+strictEqual(stringCalculator("1,5"), 6);
+strictEqual(stringCalculator("1,3,2,2"), 8);
+strictEqual(stringCalculator("150,3,2"), 155);
